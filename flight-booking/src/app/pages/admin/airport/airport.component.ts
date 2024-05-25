a5db3db9d2f87d6625cc687896de1ebf0372acac
Santiago <oenvinipas2@gmail.com>
2024-05-25 12:41:33 -0400

    airport functionality seems to work

--- flight-booking/src/app/pages/admin/airport/airport.component.ts
+++ flight-booking/src/app/pages/admin/airport/airport.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../api.service';
+
 
 @Component({
   selector: 'app-airport',
   templateUrl: './airport.component.html',
   styleUrl: './airport.component.css'
 })
-export class AirportComponent {
+export class AirportComponent implements OnInit {
+  airportList: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.getAllAirports();
+  }
+
+  getAllAirports() {
+    this.apiService.getData("GetAllAirport").subscribe((res: any) => {
+      this.airportList = res.data;
+    })
+  }
+
+  bulkUpdateAirport() {
+    this.apiService.postData("AddUpdateBulkAirports", this.airportList).subscribe((res: any) => {
+      if(res.result) {
+        alert("Bulk Update Success")
+      } else {
+        alert(res.message)
+      }
+    });
+  }
+
+  addNew() {
+    const obj = {
+      airportId: 0,
+      airportCode: "",
+      airportName: "",
+      cityId: 0,
+    }
+    this.airportList.unshift(obj);
+  }
 }
