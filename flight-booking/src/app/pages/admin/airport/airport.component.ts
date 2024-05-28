25de1374d73ba7916531c0a00d38fdebe5fb5f4a
Santiago <oenvinipas2@gmail.com>
2024-05-27 22:25:56 -0400

    doesn't work but it 'admins' should be able to add new airports to the 'db'

--- flight-booking/src/app/pages/admin/airport/airport.component.ts
+++ flight-booking/src/app/pages/admin/airport/airport.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../api.service';
 
-
 @Component({
   selector: 'app-airport',
   templateUrl: './airport.component.html',
-  styleUrl: './airport.component.css'
+  styleUrl: './airport.component.css',
 })
 export class AirportComponent implements OnInit {
   airportList: any[] = [];
@@ -17,28 +16,30 @@ export class AirportComponent implements OnInit {
   }
 
   getAllAirports() {
-    this.apiService.getData("GetAllAirport").subscribe((res: any) => {
+    this.apiService.getData('GetAllAirport').subscribe((res: any) => {
       this.airportList = res.data;
-    })
+    });
   }
 
   bulkUpdateAirport() {
-    this.apiService.postData("AddUpdateBulkAirports", this.airportList).subscribe((res: any) => {
-      if(res.result) {
-        alert("Bulk Update Success")
-      } else {
-        alert(res.message)
-      }
-    });
+    this.apiService
+      .postData('AddUpdateBulkAirports', this.airportList)
+      .subscribe((res: any) => {
+        if (res.result) {
+          alert('Bulk Update Success');
+        } else {
+          alert(res.message);
+        }
+      });
   }
 
   addNew() {
     const obj = {
       airportId: 0,
-      airportCode: "",
-      airportName: "",
+      airportCode: '',
+      airportName: '',
       cityId: 0,
-    }
+    };
     this.airportList.unshift(obj);
   }
 }
