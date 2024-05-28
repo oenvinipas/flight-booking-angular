e6e4090f02839dbde20b921f98c1bb80ea71556f
Santiago <oenvinipas2@gmail.com>
2024-05-27 22:22:35 -0400

    getting all flights from api

--- flight-booking/src/app/pages/admin/all-flights/all-flights.component.ts
+++ flight-booking/src/app/pages/admin/all-flights/all-flights.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../api.service';
 
 @Component({
   selector: 'app-all-flights',
   templateUrl: './all-flights.component.html',
   styleUrl: './all-flights.component.css'
 })
-export class AllFlightsComponent {
+export class AllFlightsComponent implements OnInit {
+  flightList: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+  ngOnInit() {
+    this.loadFlights()
+  }
+
+  loadFlights() {
+    this.apiService.getData("GetAllFlights").subscribe((res: any) => {
+      this.flightList = res.data;
+    })
+  }
 
 }
